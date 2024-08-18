b3be10d807ad682a699f0e15b7331bc25304846b
Navaneethan <navaneethan.bc@gmail.com>
2024-08-18 15:10:42 +0530

    start from here

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react'
+import Login from './components/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
+    <Login />
   )
 }
 
