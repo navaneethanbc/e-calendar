11337989cc79a72f66067519b5d05838be085af3
SivaNiroshan <sniroshan1964@gmail.com>
2024-08-19 14:08:06 +0530

    login page created

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
 
+import React from 'react'
+import blogo from'../assets/logo.png'
+import clogo from '../assets/background.jpg'
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='loginMain'>
+        <div className='header'>
+            <img className="boclogo" src={blogo} alt="boc logo" />
+            <h1>calendar</h1>
+        </div>
+        <hr />
+        <div className='container'>
+            <div className='imgcontainer'>
+                <img className='callogo' src={clogo} alt="calendar logo" />
+            </div>
+        <div className='logincontainer'>
+            <h1>Login</h1>
+            <form action="">
+                <input 
+                    type="email" 
+                    id='email' 
+                    placeholder='Email address'
+                    required
+                />
+                <input 
+                    type="password" 
+                    id='password'
+                    placeholder='Password'
+                    required
+                />
+                <button>Login</button>
+                
+                <div>
+                <a className='fp' href="#">Forgot Password</a>
+                <hr />
+                </div>
+                
+                <div className='para'>
+                    <p>New to Calendar? <a href="#">Register</a></p>
+                </div>
+            
+            </form>
+            
+        </div>
+        </div>
+            
+    </div>
   )
 }
 
