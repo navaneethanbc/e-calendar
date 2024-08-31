1818c5646dc9af8060be1662418234863972a3e9
pabasarayasiru <pabasara.yasiru@gmail.com>
2024-08-31 13:00:16 +0530

    norification route

--- client/src/components/CalendarComponent/SearchBar.jsx
+++ client/src/components/CalendarComponent/SearchBar.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import "./SearchBar.css";
 
 const SearchBar = ({
   searchQuery,
   setSearchQuery,
   startDate,
   setStartDate,
-  endDate,
-  setEndDate,
   category,
   setCategory,
   recurrenceType,
@@ -17,26 +14,26 @@ const SearchBar = ({
   onSearch,
 }) => {
   return (
-    <div className="search-bar">
+    <div className="flex items-center gap-2 p-4">
       <input
         type="text"
         placeholder="Search events..."
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
+        className="p-2 border border-gray-300 rounded-md flex-grow min-w-[150px]"
       />
       <input
         type="date"
         value={startDate}
         onChange={(e) => setStartDate(e.target.value)}
         placeholder="Start date"
+        className="p-2 border border-gray-300 rounded-md min-w-[150px] "
       />
-      <input
-        type="date"
-        value={endDate}
-        onChange={(e) => setEndDate(e.target.value)}
-        placeholder="End date"
-      />
-      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+      <select
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        className="p-2 border border-gray-300 rounded-md min-w-[150px]"
+      >
         <option value="">All Categories</option>
         <option value="Personal">Personal</option>
         <option value="Branch">Branch</option>
@@ -45,6 +42,7 @@ const SearchBar = ({
       <select
         value={recurrenceType}
         onChange={(e) => setRecurrenceType(e.target.value)}
+        className="p-2 border border-gray-300 rounded-md min-w-[150px]"
       >
         <option value="">All Recurrence Types</option>
         <option value="Non-recurring">Non-recurring</option>
@@ -56,6 +54,7 @@ const SearchBar = ({
       <select
         value={reminderType}
         onChange={(e) => setReminderType(e.target.value)}
+        className="p-2 border border-gray-300 rounded-md min-w-[150px]"
       >
         <option value="">All Reminder Types</option>
         <option value="None">None</option>
@@ -64,7 +63,10 @@ const SearchBar = ({
         <option value="1 hour before">1 hour before</option>
         <option value="1 day before">1 day before</option>
       </select>
-      <button className="search-button" onClick={onSearch}>
+      <button
+        className="p-2 px-4 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+        onClick={onSearch}
+      >
         Search
       </button>
     </div>
